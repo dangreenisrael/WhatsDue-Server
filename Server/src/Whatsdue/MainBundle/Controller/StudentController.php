26c0c63f22798be952a546dcfacf99db7dd849e0
Dan Green <dan@tlvwebdevelopment.com>
2014-09-30 15:30:23 -0400

    Minor

--- Server/src/Whatsdue/MainBundle/Controller/StudentController.php
+++ Server/src/Whatsdue/MainBundle/Controller/StudentController.php
@@ -176,6 +176,7 @@ class StudentController extends Controller{
     public function postCourseUnenrollAction($courseId){
 
         $primaryKey = $_POST['primaryKey'];
+        if ($primaryKey =="") return "No Push";
         $em = $this->getDoctrine()->getManager();
         $student = $em->getRepository('WhatsdueMainBundle:Students')->find($primaryKey);
         $course = $em->getRepository('WhatsdueMainBundle:Courses')->find($courseId);
