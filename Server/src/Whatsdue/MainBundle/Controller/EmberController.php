c6c3a1f725f804541fa9dc545e42bb71cb045514
Dan Green <dan@tlvwebdevelopment.com>
2014-11-30 16:20:49 +0200

    switched mailer to MailGun

--- Server/src/Whatsdue/MainBundle/Controller/EmberController.php
+++ Server/src/Whatsdue/MainBundle/Controller/EmberController.php
@@ -3,11 +3,22 @@
 namespace Whatsdue\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Response;
+
 
 class EmberController extends Controller
 {
     public function indexAction()
     {
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Hello Email')
+            ->setSender("accounts@whatsdueapp.com")
+            ->setTo('dan@tlvwebdevelopment.com')
+            ->setBody( 'This is a test')
+        ;
+        $this->get('mailer')->send($message);
+
         return $this->render('WhatsdueMainBundle:Ember:index.html.twig');
     }
+
 }
